7ceddec3feececb883934346f1f9ae5d56f2c1b4
Samuel Uribe <su@openbots.ai>
2020-12-23 10:24:49 -0500

    Final fixes for outlook tests

--- OpenBots.Commands.Test/OpenBots.Commands.Outlook.Test/ReplyToOutlookEmailCommandTests.cs
+++ OpenBots.Commands.Test/OpenBots.Commands.Outlook.Test/ReplyToOutlookEmailCommandTests.cs
@@ -1,12 +1,91 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Outlook;
+using OpenBots.Core.Utilities.CommonUtilities;
+using OpenBots.Engine;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Xunit;
 
 namespace OpenBots.Commands.Outlook.Test
 {
     public class ReplyToOutlookEmailCommandTests
     {
+        private AutomationEngineInstance _engine;
+        private ReplyToOutlookEmailCommand _replyToOutlookEmail;
+        private GetOutlookEmailsCommand _getOutlookEmails;
+        private DeleteOutlookEmailCommand _deleteOutlookEmail;
+
+        [Fact]
+        public void ReplysToOutlookEmail()
+        {
+            _engine = new AutomationEngineInstance(null);
+            _replyToOutlookEmail = new ReplyToOutlookEmailCommand();
+            _getOutlookEmails = new GetOutlookEmailsCommand();
+
+            _getOutlookEmails.v_SourceFolder = "TestInput";
+            _getOutlookEmails.v_Filter = "[Subject] = 'toReply'";
+            _getOutlookEmails.v_GetUnreadOnly = "No";
+            _getOutlookEmails.v_MarkAsRead = "No";
+            _getOutlookEmails.v_SaveMessagesAndAttachments = "No";
+            _getOutlookEmails.v_MessageDirectory = "";
+            _getOutlookEmails.v_AttachmentDirectory = "";
+            _getOutlookEmails.v_OutputUserVariableName = "{emails}";
+
+            _getOutlookEmails.RunCommand(_engine);
+
+            var emails = (List<MailItem>)"{emails}".ConvertUserVariableToObject(_engine);
+            MailItem email = emails[0];
+            email.StoreInUserVariable(_engine, "{email}");
+
+            _replyToOutlookEmail.v_MailItem = "{email}";
+            _replyToOutlookEmail.v_OperationType = "Reply";
+            _replyToOutlookEmail.v_Body = "replyBody";
+            _replyToOutlookEmail.v_BodyType = "Plain";
+
+            _replyToOutlookEmail.RunCommand(_engine);
+            System.Threading.Thread.Sleep(30000);
+
+            _getOutlookEmails.v_SourceFolder = "Inbox";
+            _getOutlookEmails.v_Filter = "[Subject] = 'toReply'";
+            _getOutlookEmails.v_GetUnreadOnly = "No";
+            _getOutlookEmails.v_MarkAsRead = "No";
+            _getOutlookEmails.v_SaveMessagesAndAttachments = "No";
+            _getOutlookEmails.v_MessageDirectory = "";
+            _getOutlookEmails.v_AttachmentDirectory = "";
+            _getOutlookEmails.v_OutputUserVariableName = "{emails}";
+
+            _getOutlookEmails.RunCommand(_engine);
+
+            emails = (List<MailItem>)"{emails}".ConvertUserVariableToObject(_engine);
+            email = emails[0];
+
+            resetReplyEmail(_engine);
+
+            Assert.Equal("replyBody \r\n", email.Body);
+        }
+
+        private void resetReplyEmail(AutomationEngineInstance _engine)
+        {
+            _deleteOutlookEmail = new DeleteOutlookEmailCommand();
+            _getOutlookEmails = new GetOutlookEmailsCommand();
+
+            _getOutlookEmails.v_SourceFolder = "Inbox";
+            _getOutlookEmails.v_Filter = "[Subject] = 'toReply'";
+            _getOutlookEmails.v_GetUnreadOnly = "No";
+            _getOutlookEmails.v_MarkAsRead = "No";
+            _getOutlookEmails.v_SaveMessagesAndAttachments = "No";
+            _getOutlookEmails.v_MessageDirectory = "";
+            _getOutlookEmails.v_AttachmentDirectory = "";
+            _getOutlookEmails.v_OutputUserVariableName = "{emails}";
+
+            _getOutlookEmails.RunCommand(_engine);
+
+            var emails = (List<MailItem>)"{emails}".ConvertUserVariableToObject(_engine);
+            MailItem email = emails[0];
+            email.StoreInUserVariable(_engine, "{email}");
+
+            _deleteOutlookEmail.v_MailItem = "{email}";
+            _deleteOutlookEmail.v_DeleteReadOnly = "No";
+
+            _deleteOutlookEmail.RunCommand(_engine);
+        }
     }
 }
--- OpenBots.Commands.Test/OpenBots.Commands.Outlook.Test/SendOutlookEmailCommandTests.cs
+++ OpenBots.Commands.Test/OpenBots.Commands.Outlook.Test/SendOutlookEmailCommandTests.cs
@@ -3,10 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using OpenBots.Engine;
+using OpenBots.Core.Utilities.CommonUtilities;
+using Microsoft.Office.Interop.Outlook;
 
 namespace OpenBots.Commands.Outlook.Test
 {
     public class SendOutlookEmailCommandTests
     {
+        private AutomationEngineInstance _engine;
+        private SendOutlookEmailCommand _sendOutlookEmail;
+        private GetOutlookEmailsCommand _getOutlookEmails;
+        private DeleteOutlookEmailCommand _deleteOutlookEmail;
+
+        [Fact]
+        public void SendsOutlookEmail()
+        {
+            _engine = new AutomationEngineInstance(null);
+            _sendOutlookEmail = new SendOutlookEmailCommand();
+            _getOutlookEmails = new GetOutlookEmailsCommand();
+
+            _sendOutlookEmail.v_Recipients = "openbots.test@outlook.com";
+            _sendOutlookEmail.v_Subject = "testSend";
+            _sendOutlookEmail.v_Body = "testBody";
+            _sendOutlookEmail.v_BodyType = "Plain";
+
+            _sendOutlookEmail.RunCommand(_engine);
+
+            System.Threading.Thread.Sleep(30000);
+
+            _getOutlookEmails.v_SourceFolder = "Inbox";
+            _getOutlookEmails.v_Filter = "[Subject] = 'testSend'";
+            _getOutlookEmails.v_GetUnreadOnly = "No";
+            _getOutlookEmails.v_MarkAsRead = "No";
+            _getOutlookEmails.v_SaveMessagesAndAttachments = "No";
+            _getOutlookEmails.v_MessageDirectory = "";
+            _getOutlookEmails.v_AttachmentDirectory = "";
+            _getOutlookEmails.v_OutputUserVariableName = "{emails}";
+
+            _getOutlookEmails.RunCommand(_engine);
+
+            var emails = (List<MailItem>)"{emails}".ConvertUserVariableToObject(_engine);
+            MailItem email = emails[0];
+
+            Assert.Equal("testSend", email.Subject);
+
+            resetSendEmail(_engine, email);
+        }
+
+        private void resetSendEmail(AutomationEngineInstance _engine, MailItem email)
+        {
+            _deleteOutlookEmail = new DeleteOutlookEmailCommand();
+            email.StoreInUserVariable(_engine, "{email}");
+
+            _deleteOutlookEmail.v_MailItem = "{email}";
+            _deleteOutlookEmail.v_DeleteReadOnly = "No";
+
+            _deleteOutlookEmail.RunCommand(_engine);
+        }
     }
 }
