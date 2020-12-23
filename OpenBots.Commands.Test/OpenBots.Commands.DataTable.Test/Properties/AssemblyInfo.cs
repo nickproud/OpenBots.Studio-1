4094a52142d8cb58bed5362f700090bbcd769716
Samuel Uribe <su@openbots.ai>
2020-12-23 16:02:23 -0500

    Update all test project names to plural form (Tests)

--- OpenBots.Commands.Test/OpenBots.Commands.DataTable.Test/Properties/AssemblyInfo.cs
+++ OpenBots.Commands.Test/OpenBots.Commands.DataTable.Test/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpenBots.Commands.DataTable.Test")]
+[assembly: AssemblyTitle("OpenBots.Commands.DataTable.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("OpenBots Inc")]
