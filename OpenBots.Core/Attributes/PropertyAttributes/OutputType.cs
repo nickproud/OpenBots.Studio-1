819442f6d3cd45165c5213fb00ef45563903bb85
Francesca Faerman <ff@openbots.ai>
2021-02-16 18:44:20 -0500

    added in/out attributes to core

---
+++ OpenBots.Core/Attributes/PropertyAttributes/OutputType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OpenBots.Core.Attributes.PropertyAttributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class OutputType : Attribute
+    {
+        public Type OutType { get; private set; }
+        public OutputType(Type outType)
+        {
+            OutType = outType;
+        }
+    }
+}
