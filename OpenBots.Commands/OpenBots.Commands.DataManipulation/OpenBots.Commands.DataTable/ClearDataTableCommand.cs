7510df3ed4bb1ccd28b9d6db00c7c47267abf256
openbots-aw <75852218+openbots-aw@users.noreply.github.com>
2021-03-11 10:56:20 +0500

    Added DataTable Commands (#162)
    * Added DataTable Commands
    
    - Add Data Column
    - Remove Data Column
    - Clear DataTable
    
    * Modified some attribute descriptions to match similar existing commands (AddRow, RemoveRow, etc)
    
    Co-authored-by: arslanwaraich <arslan.waraich@accelirate.com>
    Co-authored-by: Francesca Faerman <ff@openbots.ai>

---
+++ OpenBots.Commands/OpenBots.Commands.DataManipulation/OpenBots.Commands.DataTable/ClearDataTableCommand.cs
@@ -0,0 +1,63 @@
+﻿using OpenBots.Core.Attributes.PropertyAttributes;
+using OpenBots.Core.Command;
+using OpenBots.Core.Enums;
+using OpenBots.Core.Infrastructure;
+using OpenBots.Core.Properties;
+using OpenBots.Core.Utilities.CommonUtilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Windows.Forms;
+using OBDataTable = System.Data.DataTable;
+
+namespace OpenBots.Commands.DataTable
+{
+	[Serializable]
+	[Category("DataTable Commands")]
+	[Description("This command clears a DataTable of all data.")]
+	public class ClearDataTableCommand : ScriptCommand
+	{
+		[Required]
+		[DisplayName("DataTable")]
+		[Description("Enter an existing DataTable.")]
+		[SampleUsage("{vDataTable}")]
+		[Remarks("")]
+		[Editor("ShowVariableHelper", typeof(UIAdditionalHelperType))]
+		[CompatibleTypes(new Type[] { typeof(OBDataTable) })]
+		public string v_DataTable { get; set; }
+
+		public ClearDataTableCommand()
+		{
+			CommandName = "ClearDataTableCommand";
+			SelectionName = "Clear Datatable";
+			CommandEnabled = true;
+			CommandIcon = Resources.command_spreadsheet;
+		}
+
+		public override void RunCommand(object sender)
+		{
+			var engine = (IAutomationEngineInstance)sender;
+
+			var dataTableVariable = v_DataTable.ConvertUserVariableToObject(engine, nameof(v_DataTable), this);
+			OBDataTable dataTable = (OBDataTable)dataTableVariable;
+
+			dataTable.Clear();
+			dataTable.StoreInUserVariable(engine, v_DataTable, nameof(v_DataTable), this);
+		}
+
+		public override List<Control> Render(IfrmCommandEditor editor, ICommandControls commandControls)
+		{
+			base.Render(editor, commandControls);
+
+			RenderedControls.AddRange(commandControls.CreateDefaultInputGroupFor("v_DataTable", this, editor));
+
+			return RenderedControls;
+		}
+
+		public override string GetDisplayValue()
+		{
+			return base.GetDisplayValue() + $" ['{v_DataTable}']";
+		}
+	}
+}
