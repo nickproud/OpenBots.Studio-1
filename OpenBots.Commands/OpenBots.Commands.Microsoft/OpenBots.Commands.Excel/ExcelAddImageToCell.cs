1aacbb29f6f04ec112e4b634390c52d0d66097ae
arslanwaraich <arslan.waraich@accelirate.com>
2021-03-17 16:46:41 +0500

    Added Command Add image to Excel Cell

--- OpenBots.Commands/OpenBots.Commands.Microsoft/OpenBots.Commands.Excel/ExcelAddImageToCell.cs
+++ OpenBots.Commands/OpenBots.Commands.Microsoft/OpenBots.Commands.Excel/ExcelAddImageToCell.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Office.Interop.Excel;
+﻿using Newtonsoft.Json;
 using OpenBots.Core.Attributes.PropertyAttributes;
 using OpenBots.Core.Command;
 using OpenBots.Core.Enums;
@@ -11,14 +11,16 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Windows.Forms;
 using Application = Microsoft.Office.Interop.Excel.Application;
+using Microsoft.Office.Core;
+using Microsoft.Office.Interop.Excel;
 
 namespace OpenBots.Commands.Excel
 {
-	[Serializable]
-	[Category("Excel Commands")]
-	[Description("This command sets the value of a specific cell in an Excel Worksheet.")]
-	public class ExcelWriteCellCommand : ScriptCommand
-	{
+    [Serializable]
+    [Category("Excel Commands")]
+    [Description("This command adds an image to excel cell.")]
+    public class ExcelAddImageToCell : ScriptCommand
+    {
 		[Required]
 		[DisplayName("Excel Instance Name")]
 		[Description("Enter the unique instance that was specified in the **Create Application** command.")]
@@ -28,27 +30,28 @@ namespace OpenBots.Commands.Excel
 		public string v_InstanceName { get; set; }
 
 		[Required]
-		[DisplayName("Cell Value")]
-		[Description("Enter the text value that will be set in the selected cell.")]
-		[SampleUsage("Hello World || {vText}")]
-		[Remarks("")]
-		[Editor("ShowVariableHelper", typeof(UIAdditionalHelperType))]
-		[CompatibleTypes(null, true)]
-		public string v_TextToSet { get; set; }
-
-		[Required]
 		[DisplayName("Cell Location")]
-		[Description("Enter the location of the cell to set the text value.")]
+		[Description("Enter the location of the cell to add image.")]
 		[SampleUsage("A1 || {vCellLocation}")]
 		[Remarks("")]
 		[Editor("ShowVariableHelper", typeof(UIAdditionalHelperType))]
 		[CompatibleTypes(null, true)]
 		public string v_CellLocation { get; set; }
 
-		public ExcelWriteCellCommand()
+		[Required]
+		[DisplayName("Image Path")]
+		[Description("Enter or Select the path to the image file.")]
+		[SampleUsage(@"C:\temp\image.png || {vFilePath} || {ProjectPath}\image.png")]
+		[Remarks("")]
+		[Editor("ShowVariableHelper", typeof(UIAdditionalHelperType))]
+		[Editor("ShowFileSelectionHelper", typeof(UIAdditionalHelperType))]
+		[CompatibleTypes(null, true)]
+		public string v_ImagePath { get; set; }
+
+		public ExcelAddImageToCell()
 		{
-			CommandName = "ExcelWriteCellCommand";
-			SelectionName = "Write Cell";
+			CommandName = "ExcelAddImageToCell";
+			SelectionName = "Add Image To Cell";
 			CommandEnabled = true;
 			CommandIcon = Resources.command_spreadsheet;
 
@@ -61,11 +64,15 @@ namespace OpenBots.Commands.Excel
 			var engine = (IAutomationEngineInstance)sender;
 			var excelObject = v_InstanceName.GetAppInstance(engine);
 			var vTargetAddress = v_CellLocation.ConvertUserVariableToString(engine);
-			var vTargetText = v_TextToSet.ConvertUserVariableToString(engine);
+			var vImagePath = v_ImagePath.ConvertUserVariableToString(engine);
 			var excelInstance = (Application)excelObject;
 
 			Worksheet excelSheet = excelInstance.ActiveSheet;
-			excelSheet.Range[vTargetAddress].Value = vTargetText;           
+			Range oRange = excelSheet.Range[vTargetAddress];
+			float left = (float)((double)oRange.Left);
+			float top = (float)((double)oRange.Top);
+			System.Drawing.Image img = System.Drawing.Image.FromFile(vImagePath);
+			excelSheet.Shapes.AddPicture(vImagePath, MsoTriState.msoFalse, MsoTriState.msoCTrue, left, top, img.Width, img.Height);
 		}
 
 		public override List<Control> Render(IfrmCommandEditor editor, ICommandControls commandControls)
@@ -73,15 +80,15 @@ namespace OpenBots.Commands.Excel
 			base.Render(editor, commandControls);
 
 			RenderedControls.AddRange(commandControls.CreateDefaultInputGroupFor("v_InstanceName", this, editor));
-			RenderedControls.AddRange(commandControls.CreateDefaultInputGroupFor("v_TextToSet", this, editor));
 			RenderedControls.AddRange(commandControls.CreateDefaultInputGroupFor("v_CellLocation", this, editor));
+			RenderedControls.AddRange(commandControls.CreateDefaultInputGroupFor("v_ImagePath", this, editor));
 
 			return RenderedControls;
 		}
 
 		public override string GetDisplayValue()
 		{
-			return base.GetDisplayValue() + $" [Write '{v_TextToSet}' to Cell '{v_CellLocation}' - Instance Name '{v_InstanceName}']";
+			return base.GetDisplayValue() + $" [Add Image '{v_ImagePath}' to Cell '{v_CellLocation}' - Instance Name '{v_InstanceName}']";
 		}
 	}
 }
