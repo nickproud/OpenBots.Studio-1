9700957e137418123f6b2b089086d2f19436a831
Francesca Faerman <ff@openbots.ai>
2021-03-30 09:53:19 -0400

    added one more namespace helper method

--- OpenBots.Core/Utilities/CommonUtilities/NamespaceMethods.cs
+++ OpenBots.Core/Utilities/CommonUtilities/NamespaceMethods.cs
@@ -9,7 +9,7 @@ namespace OpenBots.Core.Utilities.CommonUtilities
 {
     public static class NamespaceMethods
     {
-        public static Dictionary<string, AssemblyReference> GetNamespaces(IAutomationEngineInstance engine)
+        public static Dictionary<string, AssemblyReference> GetNamespacesDict(IAutomationEngineInstance engine)
         {
             return engine.AutomationEngineContext.ImportedNamespaces;
         }
@@ -20,12 +20,35 @@ namespace OpenBots.Core.Utilities.CommonUtilities
             {
                 if (engine.AutomationEngineContext.ImportedNamespaces.TryGetValue(namespaceKey, out AssemblyReference assemblyReference))
                 {
-                    return AppDomain.CurrentDomain.GetAssemblies().Where(x => x.GetName().Name == assemblyReference.AssemblyName && 
-                                                                              x.GetName().Version == Version.Parse(assemblyReference.AssemblyVersion))
-                                                                  .FirstOrDefault();
+                    return AppDomain.CurrentDomain.GetAssemblies()
+                                                  .Where(x => x.GetName().Name == assemblyReference.AssemblyName && 
+                                                              x.GetName().Version == Version.Parse(assemblyReference.AssemblyVersion))
+                                                  .FirstOrDefault();
                 }
                 
                 throw new Exception($"Assembly for Namespace '{namespaceKey}' not found!");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static List<Assembly> GetAssemblies(IAutomationEngineInstance engine)
+        {
+            try
+            {
+                if (engine.AutomationEngineContext.ImportedNamespaces != null && engine.AutomationEngineContext.ImportedNamespaces.Count > 0)
+                {
+                    return engine.AutomationEngineContext.ImportedNamespaces.Select(x => AppDomain.CurrentDomain.GetAssemblies()
+                                                                                                                .Where(y => y.GetName().Name == x.Value.AssemblyName &&
+                                                                                                                            y.GetName().Version == Version.Parse(x.Value.AssemblyVersion))
+                                                                                                                .FirstOrDefault())
+                                                                            .Distinct()
+                                                                            .ToList();
+                }
+
+                throw new Exception($"ImportedNamespaces is null or empty!");
             }
             catch (Exception ex)
             {
