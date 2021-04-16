0f1ade5d0fb9211f0f2fad7995c75c75f25c96d9
Samuel Uribe <su@openbots.ai>
2021-04-16 14:30:54 -0400

    Update SetVariableValue

--- OpenBots.Core/Utilities/CommonUtilities/VariableMethods.cs
+++ OpenBots.Core/Utilities/CommonUtilities/VariableMethods.cs
@@ -559,7 +559,13 @@ namespace OpenBots.Core.Utilities.CommonUtilities
         }
         public static void SetVariableValue(this object newVal, IAutomationEngineInstance engine, string varName, Type varType)
         {
-            engine.AutomationEngineContext.EngineScriptState.Variables.Where(x => x.Name == varName).FirstOrDefault().Value = Convert.ChangeType(newVal, varType);
+            engine.AutomationEngineContext.EngineScriptState.Variables.Where(x => x.Name == varName).FirstOrDefault().Value = newVal;
+            var existingVar = engine.AutomationEngineContext.Variables.Where(x => x.VariableName == varName).FirstOrDefault();
+            if (existingVar != null)
+                existingVar.VariableValue = newVal;
+            var existingArg = engine.AutomationEngineContext.Arguments.Where(x => x.ArgumentName == varName).FirstOrDefault();
+            if (existingArg != null)
+                existingArg.ArgumentValue = newVal;
         }
 
         public static void SetVariableValue(this object newVal, IAutomationEngineInstance engine, string varName, string parameterName, ScriptCommand parent)
