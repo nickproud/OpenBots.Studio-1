0efed7981c84df1beefea4fab9477bcbcd59dc67
Francesca Faerman <ff@openbots.ai>
2021-05-10 11:12:29 -0400

    changed error message to show all errors

--- OpenBots.Studio/UI/Forms/frmCommandEditor.cs
+++ OpenBots.Studio/UI/Forms/frmCommandEditor.cs
@@ -415,7 +415,10 @@ namespace OpenBots.UI.Forms
                 if (result.Success)
                     return isAllValid;
                 else
-                    errorMessage = result.Diagnostics.ToList().Where(x => x.DefaultSeverity == DiagnosticSeverity.Error).FirstOrDefault()?.ToString();
+                {
+                    var errorMessages = result.Diagnostics.ToList().Where(x => x.DefaultSeverity == DiagnosticSeverity.Error).Select(x => x.ToString()).ToArray();
+                    errorMessage = string.Join(Environment.NewLine, errorMessages);
+                }
             }
  
             isAllValid = false;
