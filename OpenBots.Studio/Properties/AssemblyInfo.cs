1874c4aa56f6cfdfb8ea6370879eaad7a9ceddc7
Nick <nick.proud@nexbotix.com>
2022-01-02 21:07:28 +0000

    Changed assembly info and logo

--- OpenBots.Studio/Properties/AssemblyInfo.cs
+++ OpenBots.Studio/Properties/AssemblyInfo.cs
@@ -18,12 +18,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpenBots Studio")]
-[assembly: AssemblyDescription("Open Source Automation for All")]
+[assembly: AssemblyTitle("NexBots Studio")]
+[assembly: AssemblyDescription("Intelligent Automation As A Service")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("OpenBots Inc")]
-[assembly: AssemblyProduct("OpenBots Studio")]
-[assembly: AssemblyCopyright("(C) OpenBots Inc 2020-2021")]
+[assembly: AssemblyCompany("Nexbotix Ltd")]
+[assembly: AssemblyProduct("NexBots Studio")]
+[assembly: AssemblyCopyright("(C) NexBotix Ltd 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -44,6 +44,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.5.2.0")]
-[assembly: AssemblyFileVersion("1.5.2.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 
